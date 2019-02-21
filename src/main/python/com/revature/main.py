8626859efd05b9bb764b14c4b845d2e841f4827d
Phillip Tran <Philtneo1@localhost.localdomain>
2019-02-21 00:49:11 -0500

    updated main

--- src/main/python/com/revature/main.py
+++ src/main/python/com/revature/main.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
-
 '''
 This is your main script, this should call several other scripts within your packages.
 '''
-def main():
-	print('TO-DO')
+from controller import controller
 
+def main():
+	print('Welcome to the bank application')
+	controller.menu()
+
+	
+	
 if __name__ == '__main__':
 	main()
